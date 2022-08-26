cf7211755ae17f85562572628edc84e92e99d22f
Artyom <asatryanartyom.1989@gmail.com>
2022-08-26 20:22:24 +0300

    DZ_SEM3C#

---
+++ Drugoye_Resheniye_DZ3/Program.cs
@@ -0,0 +1,11 @@
+﻿////Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+Console.WriteLine("Введите чило:");
+int n = Convert.ToInt32 (Console.ReadLine());
+int index = 1;
+while (index<=n)
+{
+Console.WriteLine(Math.Pow(index,3));
+index++;
+}
